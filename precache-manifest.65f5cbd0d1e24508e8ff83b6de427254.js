9185dc2b800a91ba248bb9a3687b4eb491f4fce4
Daksh Doshi <ddaksh.2711@gmail.com>
2020-11-24 10:09:00 +0530

    Updates

---
+++ precache-manifest.65f5cbd0d1e24508e8ff83b6de427254.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40066ad3c9971368254e8f62f287f16e",
+    "url": "/covid-welfare-frontend/index.html"
+  },
+  {
+    "revision": "fefa7d9fd3bd16958436",
+    "url": "/covid-welfare-frontend/static/css/main.693185c6.chunk.css"
+  },
+  {
+    "revision": "7cac58ea552cc4b3f3ed",
+    "url": "/covid-welfare-frontend/static/js/2.56b03a37.chunk.js"
+  },
+  {
+    "revision": "f68c7676fb964371e22a5bb57ab60409",
+    "url": "/covid-welfare-frontend/static/js/2.56b03a37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fefa7d9fd3bd16958436",
+    "url": "/covid-welfare-frontend/static/js/main.a16836fd.chunk.js"
+  },
+  {
+    "revision": "2db1e43cac010805372b",
+    "url": "/covid-welfare-frontend/static/js/runtime-main.6f6f53f0.js"
+  },
+  {
+    "revision": "03884365f2a5fa8fdfffdc13a860639f",
+    "url": "/covid-welfare-frontend/static/media/doctors.03884365.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/covid-welfare-frontend/static/media/logo.5d5d9eef.svg"
+  }
+]);
